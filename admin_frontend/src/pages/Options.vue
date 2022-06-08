87405571e6f348666be8075541ae18823bcc1522
the_nza <the_nza@protonmail.com>
2022-06-08 21:08:04 +0300

    Use settings component for options page.

--- admin_frontend/src/pages/Options.vue
+++ admin_frontend/src/pages/Options.vue
@@ -1,10 +1,15 @@
 <template>
-  <Navigation />
-  <h1>This is edit options page</h1>
+	<PageView>
+		<PageBody>
+			<Settings/>
+		</PageBody>
+	</PageView>
 </template>
 
 <script setup lang="ts">
-import Navigation from '../components/Navigation.vue';
+import PageView from '../components/PageView.vue';
+import PageBody from '../components/PageBody.vue';
+import Settings from '../components/Settings.vue';
 </script>
 
 <style scoped>
