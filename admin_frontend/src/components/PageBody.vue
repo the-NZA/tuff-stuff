9004d985cb202dd30e0586da2294dd1c2033a24e
the_nza <the_nza@protonmail.com>
2022-06-08 21:07:33 +0300

    Add page body helper component.

---
+++ admin_frontend/src/components/PageBody.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="pageBody">
+		<slot></slot>
+	</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.pageBody {
+	background-color: rgb(var(--white));
+	padding: var(--offset);
+	border-radius: var(--offset_half);
+	box-shadow: var(--neoShadow);
+}
+</style>
