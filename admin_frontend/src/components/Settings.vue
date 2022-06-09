b8b9442bf6b2e231fea28040716aca0c822ab188
the_nza <the_nza@protonmail.com>
2022-06-09 14:25:09 +0300

    Implement settings component and options page at once.

--- admin_frontend/src/components/Settings.vue
+++ admin_frontend/src/components/Settings.vue
@@ -1,56 +1,115 @@
 <template>
 	<div class="settings">
+		<Loader v-if="isLoading"/>
+
 		<h2 class="settings__title">Настройки сайта</h2>
 
-		<div class="settings__row">
-			<label for="emailSetting" class="settings__label">Название</label>
+		<div v-for="opt in options" class="settings__row">
+			<label :for="opt.name + `Setting`" class="settings__label">{{ opt.title }}</label>
 
 			<p class="settings__snippet">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, voluptatibus?
+				{{ helperText[opt.name].snippet }}
 			</p>
 
-			<input type="text" id="emailSetting" class="settings__input" placeholder="Название">
-		</div>
-
-		<div class="settings__row">
-			<label for="phoneSetting" class="settings__label">Название</label>
-
-			<p class="settings__snippet">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, voluptatibus?
-			</p>
-
-			<input type="text" id="phoneSetting" class="settings__input" placeholder="Название">
-		</div>
-
-		<div class="settings__row">
-			<label for="telegramSetting" class="settings__label">Название</label>
-
-			<p class="settings__snippet">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, voluptatibus?
-			</p>
-
-			<input type="text" id="telegramSetting" class="settings__input" placeholder="Название">
+			<input v-model="opt.value"
+			       @input="reset"
+			       :id="opt.name + `Setting`"
+			       type="text"
+			       class="settings__input"
+			       placeholder="Название">
 		</div>
 
 		<div class="settings__footer">
-			<button class="settings__save">Сохранить</button>
+			<button
+				@click="save"
+				class="settings__save">Сохранить
+			</button>
 
-			<div v-if="false" class="settings__error">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores error esse harum ipsam
-				necessitatibus nisi provident quibusdam? Cumque debitis magnam modi molestiae mollitia quod, reiciendis
-				reprehenderit rerum sint temporibus.
+			<div v-if="isError" class="settings__error">
+				{{ message }}
 			</div>
 
-			<div v-if="true" class="settings__success">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolores error esse harum ipsam
-				necessitatibus nisi provident quibusdam? Cumque debitis magnam modi molestiae mollitia quod, reiciendis
-				reprehenderit rerum sint temporibus.
+			<div v-if="isSuccess" class="settings__success">
+				{{ message }}
 			</div>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
+import Loader from './Loader.vue';
+import {onBeforeMount} from "vue";
+import HTTP from "../util/HTTP";
+import {Option} from "../types/Option";
+import {Response} from "../types/response";
+import {reactive, ref} from "vue";
+
+const helperText = {
+	email: {
+		snippet: "Введите адрес электронной почты",
+		placeholder: "test@test.com"
+	},
+	phone: {
+		snippet: "Номер телефона для обратной связи",
+		placeholder: "+79999999999"
+	},
+	telegram: {
+		snippet: "Ник телеграм для обратной связи без @",
+		placeholder: "test_nickname"
+	},
+	whatsapp: {
+		snippet: "Номер WhatsApp для обратной связи",
+		placeholder: "+79999999999"
+	}
+};
+
+const options = reactive<Option[]>([]);
+const message = ref("");
+
+// State flags
+const isLoading = ref(true);
+const isError = ref(false);
+const isSuccess = ref(false);
+
+// Reset flags and message
+const reset = () => {
+	isError.value = false;
+	isSuccess.value = false;
+	message.value = "";
+}
+
+// Load data from server
+onBeforeMount(async () => {
+	try {
+		const res = await HTTP.get<Response<Option[]>>("/api/v1/option")
+		options.splice(0, options.length, ...res.data.data);
+	} catch (err) {
+		isError.value = true;
+		message.value = "Не удалось данные с сервера";
+	}
+
+	isLoading.value = false;
+});
+
+// Save data to server
+const save = async () => {
+	isLoading.value = true;
+	isError.value = false;
+	isSuccess.value = false;
+
+	try {
+		await HTTP.put<Response<Option[]>>("/api/v1/option", JSON.stringify(options));
+
+		isSuccess.value = true;
+		message.value = "Настройки сохранены";
+	} catch (err) {
+		isError.value = true;
+		message.value = "Не удалось сохранить настройки";
+	}
+
+	isLoading.value = false;
+}
+
 </script>
 
 <style scoped>
@@ -137,6 +196,7 @@
 	color: rgb(var(--white));
 	font-weight: bold;
 	border-radius: var(--offset_eight);
+	border: 2px solid rgb(var(--red));
 }
 
 .settings__success {
@@ -147,8 +207,7 @@
 
 	font-weight: bold;
 	border-radius: var(--offset_eight);
+	border: 2px solid rgb(var(--light-orange));
 	text-shadow: 0 0 1px rgba(var(--gray), var(--alpha50));
 }
-
-
 </style>
