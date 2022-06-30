148440d24a3bd829746e05f382c58879013e2769
the_nza <the_nza@protonmail.com>
2022-06-30 16:58:23 +0300

    Add centralized store for message and success messages.

--- admin_frontend/src/components/ModalCardEditor.vue
+++ admin_frontend/src/components/ModalCardEditor.vue
@@ -18,6 +18,7 @@
 					<!--					@input="reset"-->
 					<input
 						v-model="editableCard.title"
+						@input="msgStore.Reset"
 						type="text"
 						class="editPage__input"
 						id="modalCardTitle"
@@ -40,6 +41,14 @@
 					class="modalEditor__save">
 					Сохранить
 				</button>
+
+				<div v-if="msgStore.IsModalError" class="modalEditor__error">
+					{{ msgStore.ModalErrorMessage }}
+				</div>
+
+				<div v-if="msgStore.IsModalSuccess" class="modalEditor__success">
+					{{ msgStore.ModalSuccessMessage }}
+				</div>
 			</div>
 		</div>
 
@@ -48,10 +57,13 @@
 
 <script setup lang="ts">
 import {computed, onBeforeMount, ref} from "vue";
+import {useMessageStore} from "../store/message";
 import MultiText from "./MultiText.vue";
 import {Card} from "../types/Card";
 import {CardType} from "../types/CardType";
 import NameByCardType from "../util/NameByCardType";
+
+const msgStore = useMessageStore();
 
 const editableCard = ref<Card>(<Card>{});
 
@@ -72,11 +84,17 @@ const props = defineProps<{
 }>()
 
 const close = () => {
+	msgStore.Reset();
+
 	emits("close");
 }
 
 const save = () => {
+	msgStore.Reset();
+
 	console.log("saved");
+
+	msgStore.SetModalSuccess("Карточка сохранена");
 }
 
 onBeforeMount(() => {
@@ -195,6 +213,13 @@ onBeforeMount(() => {
 	margin-bottom: var(--offset);
 }
 
+.modalEditor__footer {
+	display: grid;
+	grid-template-columns: max-content 1fr;
+	align-items: center;
+	gap: var(--offset_twice);
+}
+
 .modalEditor__save {
 	background-color: rgb(var(--white));
 	color: rgb(var(--purple));
@@ -212,6 +237,27 @@ onBeforeMount(() => {
 
 .modalEditor__save:hover {
 	background-color: rgb(var(--purple));
+	color: rgb(var(--white));
+}
+
+.modalEditor__error,
+.modalEditor__success {
+	font-weight: bold;
+	border: 2px solid rgb(var(--gray));
+	border-radius: var(--offset_quarter);
+
+	padding: var(--offset_half);
+}
+
+.modalEditor__error {
+	border-color: rgb(var(--red));
+	background-color: rgb(var(--red));
+	color: rgb(var(--white));
+}
+
+.modalEditor__success {
+	border-color: rgb(var(--light-orange));
+	background-color: rgb(var(--light-orange));
 	color: rgb(var(--white));
 }
 </style>
---
+++ admin_frontend/src/store/message.ts
@@ -0,0 +1,52 @@
+import {defineStore} from "pinia";
+
+export const useMessageStore = defineStore("message", {
+	state: () => ({
+		errorMessage: "",
+		isError: false,
+		modalErrorMessage: "",
+		isModalError: false,
+		modalSuccessMessage: "",
+		isModalSuccess: false,
+		successMessage: "",
+		isSuccess: false,
+	}),
+	getters: {
+		ErrorMessage: (state) => state.errorMessage,
+		IsError: (state) => state.isError,
+		SuccessMessage: (state) => state.successMessage,
+		IsSuccess: (state) => state.isSuccess,
+		ModalErrorMessage: (state) => state.modalErrorMessage,
+		IsModalError: (state) => state.isModalError,
+		ModalSuccessMessage: (state) => state.modalSuccessMessage,
+		IsModalSuccess: (state) => state.isModalSuccess,
+	},
+	actions: {
+		SetError(message: string) {
+			this.errorMessage = message;
+			this.isError = true;
+		},
+		SetSuccess(message: string) {
+			this.successMessage = message;
+			this.isSuccess = true;
+		},
+		SetModalError(message: string) {
+			this.modalErrorMessage = message;
+			this.isModalError = true;
+		},
+		SetModalSuccess(message: string) {
+			this.modalSuccessMessage = message;
+			this.isModalSuccess = true;
+		},
+		Reset() {
+			this.errorMessage = "";
+			this.isError = false;
+			this.successMessage = "";
+			this.isSuccess = false;
+			this.modalErrorMessage = "";
+			this.isModalError = false;
+			this.modalSuccessMessage = "";
+			this.isModalSuccess = false;
+		}
+	}
+})
