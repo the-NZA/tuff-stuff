2960742e4f8d6843d02d0b529cf323899eef11fc
the_nza <the_nza@protonmail.com>
2022-08-09 14:06:06 +0300

    Add copy plugin and setup it. Exclude some folders from git index.

--- admin_frontend/vite.config.ts
+++ admin_frontend/vite.config.ts
@@ -1,8 +1,22 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import copy from "rollup-plugin-copy"
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
 	base: "/admin/",
+	build: {
+		rollupOptions: {
+			plugins: [
+				copy({
+					hook: "buildEnd",
+					targets: [
+						// Copy all static files to project root/admin_static
+						{src: "dist/*", dest: "../admin_static/"},
+					]
+				}),
+			],
+		}
+	}
 })
